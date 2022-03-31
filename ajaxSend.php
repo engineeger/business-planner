bfbf78b3867e771b4f4f2b9009d27b34233c798b
ExorCistWeb <vladimirovich.aleksei.ru@gmail.com>
2022-03-31 23:19:18 +0600

    ph2p

--- ajaxSend.php
+++ ajaxSend.php
@@ -1,26 +1,19 @@
-
 <?php
-
 $to      = 'vladimirovich.aleksei.ru@gmail.com';
 $subject = 'the subject';
 $headers = 'X-Mailer: PHP/' . phpversion();
-    
-
-if (isset($_POST['send'])){
 $message = "С формы\n Телефон: " . $_POST['phone'] . "\n";
 $message .= "Имя: " . $_POST['fio'] . "\n";
 $message .= "Сообщение: " . $_POST['comment'] . "\n";
 
-echo $message;
-die();
-// $r = mail($to, $subject, $message, $headers);
+$r = mail($to, $subject, $message, $headers);
 
-// if($r){
-//     header("Location: ".$_SERVER['HTTP_REFERER']);
-//     echo $message;
-//     die();
-// }
-// }else{
-//     echo $message;
-//     header("Location: ".$_SERVER['HTTP_REFERER']);
-// }
+if($r){
+    header("Location: ".$_SERVER['HTTP_REFERER']);
+    echo true;
+    die();
+}else{
+    header("Location: ".$_SERVER['HTTP_REFERER']);
+    echo false;
+}
+?>
